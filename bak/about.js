6314e473183878aac38fdda88e0b27a83b0a5f9e
Omar Siam <Omar.Siam@oeaw.ac.at>
2017-06-06 17:49:45 +0200

    Initial check in website.

---
+++ bak/about.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function() { 
+
+var divs= [$('#ziele'), $('#help'), $('#geschichte'), $('#impressum')];
+var go2s= [$('#go2ziele'), $('#go2help'), $('#go2geschichte'), $('#go2impressum')]; 
+
+for (a in divs) { 
+  $( divs[a] ).hide(); 
+  $( divs[0] ).show(); 
+  } 
+
+for (i in go2s) {
+$(go2s[i]).click(
+  function () {  
+    for (a in divs) { $( divs[a] ).hide(); } 
+    $('#'+this.name ).show();
+    }
+  );
+  }
+});
---
+++ jb_scripts.js
@@ -0,0 +1,96 @@
+﻿$(document).ready(function() {
+
+var navs= $ ( '#navbar_items a' );
+var controls= $ ( '.control' );
+var slides= $ ( '.slide' );
+var item = 0;
+
+function whichItem(e) { 
+  for (i = 0; i < 3; i++) {
+  if ( e == controls[i] || e == navs[i]) {
+      item= i;   
+      }
+    }
+  } 
+function toggleNav() {
+  $ ( slides ).addClass( 'hide' ); 
+  $ ( navs ).removeClass( 'hilite' ); 
+  $ ( controls ).removeClass( 'hilite' ); 
+  whichItem(this); 
+  $ ( slides[item] ).removeClass( 'hide' );     
+  $ ( navs[item] ).addClass( 'hilite' );    
+  $ ( controls[item] ).addClass( 'hilite' ); 
+  }
+$ ( controls ).click( toggleNav ); 
+$ ( navs ).click( toggleNav ); 
+
+//////// ABOUT //////
+
+
+var divs= [$('#ziele'), $('#help'), $('#geschichte'), $('#impressum')];
+var go2s= [$('#go2ziele'), $('#go2help'), $('#go2geschichte'), $('#go2impressum')]; 
+
+for (a in divs) { 
+  $( divs[a] ).hide(); 
+  $( divs[0] ).show(); 
+  } 
+
+for (i in go2s) {
+$(go2s[i]).click(
+  function () {  
+    for (a in divs) { $( divs[a] ).hide(); } 
+    $('#'+this.name ).show();
+    }
+  );
+  }
+
+//////// SCHLAGWORTE //////
+
+var toggleAnd = $('.andOr').click(
+  function() { if (this.innerHTML== 'AND') this.innerHTML='OR'; 
+               else this.innerHTML= 'AND'; }
+  );
+  
+var hideResults= $('.showResults').hide();
+var toggleResults= $('.suchOptionen a').click(
+  function() {  
+    $('.showResults').toggle('slow');    
+    $('#erklärung').toggleClass('erklärung');
+    
+    }
+  );
+var hideEntry= $('.showEntry').hide();
+var toggleEntry= $('#showList a').click(
+  function() {  
+    $('.showEntry').toggle('slow');       
+    }
+  );
+
+  
+$ ( '.schlagworte li li ol' ).hide ();
+var as= $ ( '.schlagworte a' );
+for (i in as) {
+  as[i].innerHTML= Math.ceil(Math.random()*10000);
+  }
+
+var showSublist =  $ ( '.sup' ).click( 
+  function ( ) { 
+    $ ( this ).nextAll( 'ol' ).toggle('fast');
+    $ ( this ).toggleClass( 'close' );     
+    }
+  ); 
+var sup= $ ( 'div.schlagworte .sup' );
+var showAll =  $ ( '#aO' ).click( 
+  function ( ) { 
+    $ ( '.schlagworte li li ol' ).show ();
+    $ ( sup ).addClass( 'close' );     
+    }
+  );  
+var closeAll =  $ ( '#aC' ).click( 
+  function ( ) { 
+    $ ( '.schlagworte li li ol' ).hide ();
+    $ ( '.schlagworte .sup' ).removeClass( 'close' );     
+    }
+  ); 
+ 
+});
