175577d0222858776a46bd203aa0b81b164f92fe
Omar Siam <Omar.Siam@oeaw.ac.at>
2024-05-06 16:07:50 +0200

    On build initialize cache

--- deployment/initial.sh
+++ deployment/initial.sh
@@ -37,3 +37,7 @@ curl -LOJ https://arche.acdh.oeaw.ac.at/api/44309
 
 sed -i "s~../webapp/japbib-web/~${BUILD_DIR:-../webapp/japbib-web}/~g" deploy-japbib-web-content.bxs
 ./execute-basex-batch.sh ${BUILD_DIR:-../webapp/japbib-web}/deployment/deploy-japbib-web-content $1
+
+# initialize cache
+curl -o /dev/null http://localhost:8984/japbib-web/sru/refresh-scans
+curl -o /dev/null http://localhost:8984/japbib-web/thesaurus?x-mode=refresh
